55629cfe720eec66aebd4490a7e9126f08b4a78c
WalterScimone <accirasi@protonmail.com>
2021-07-21 12:04:54 -0400

    js modifications...

--- public/index.js
+++ public/index.js
@@ -1,36 +1,75 @@
 $(function() {
-    // expose socket client...
+    // expose our socket client
     var socket = io();
     
     // handle and submit new chat messages to server...
     $("form").submit(function(e) {
-      e.preventDefault(); // prevents reloading page...
-      socket.emit("chat message", $("#m").val());
+      e.preventDefault(); // prevents reloading...
+      var msg = $("#m").val();
+      if(msg.includes("ℏ")) { 
+        alert("sorry, you're unable to send messages with ℏ in them!");
+      }
+      else { 
+        socket.emit("chat message", $("#m").val());
+      }
       $("#m").val("");
       return false;
     });
   
     // listen for new chat messages from our server...
-    // these will all be sent over the Hedera consensus service...
+    // these will all sent over the Hedera consensus service!
     socket.on("chat message", function(msg) {
+  
+      // split message by a specific, special character...
+      var splitMsg = msg.split("ℏ");
+  
+      // grab the specifically formatted message...
+      var operatorId = splitMsg[0]; 
+      var clientId = splitMsg[1].slice(0,6) + "..";
+      var msg = splitMsg[2];
+      var sequenceNumber = splitMsg[3];
+      var trimmedHash = "runningHash: "+splitMsg[4].slice(0,6) + "..";
+      var trimmedTimestamp = splitMsg[5].slice(0,25);
+    
+      // grab & trim our topic ID
+      var topicId = document.getElementById("topic-id");
+      var idString = topicId.innerHTML.substring(7, topicId.length);
+    
+      // append the split message to the HTML in pieces...
       $("#messages").append(
         $("<li>").addClass("new-message").append(
-          $("<p>").text(msg).addClass("message-content")
-        )
-      );
+          $("<div>").addClass("message").append( 
+            $("<p>").text(operatorId+"@"+clientId).addClass("client")).append(
+              $("<div>").addClass("message-body").append( 
+                $("<div>").text(msg).addClass("message-content")).append(
+                $("<div>").text(trimmedTimestamp).addClass("message-timestamp")))).append(
+          $("<div>").addClass("meta").append( 
+            $("<p>").text("sequence: "+ sequenceNumber).addClass("details")).append(
+            $("<p>").text(trimmedHash).addClass("details")).append(
+            $("<a>").text("view transaction").addClass("details")
+              .attr("target", "_blank")
+              .attr("href", `https://explorer.kabuto.sh/testnet/topic/${idString}/message/${sequenceNumber}`))));
+    
+      // update the current sequence number...
+      $("#sequence-number").text("last message sequence number: " + sequenceNumber + "  ");
     });
   
     // listen for new client connections from server...
     socket.on("connect message", function(msg) {
-      // send new connection message to other clients...
+      // send new connection message...
+      var splitMsg = msg.split("ℏ");
       $("#messages").append(
-        $("<li>").text('new connection: ' + msg).addClass("new-connection"));
+        $("<li>").text('new connection: ' + splitMsg[0] + "@" + splitMsg[1]).addClass("new-connection"));
+      // update clients topic id...
+      var topicId = document.getElementById("topic-id");
+      topicId.innerHTML = "Topic: " + splitMsg[2];
     });
     
     // listen for client disconnections from server...
     socket.on("disconnect message", function(msg) {
-      // send new disconnection message to other clients...
+      // send new disconnection message...
+      var splitMsg = msg.split("ℏ");
       $("#messages").append(
-        $("<li>").text(msg + ' has disconnected').addClass("disconnection"));
+        $("<li>").text(splitMsg[0] + "@" + splitMsg[1] + ' has disconnected').addClass("disconnection"));
     });
   });
--- server.js
+++ server.js
@@ -13,24 +13,24 @@ function runChat() {
   app.use(express.static("public"));
 
   // random port setup for express...
-  http.listen(0, function() {
+  http.listen(0, function () {
     const randomInstancePort = http.address().port;
     open("http://localhost:" + randomInstancePort);
   });
 
   // when a new socket.io client connects to this server...
-  io.on("connection", function(client) {
+  io.on("connection", function (client) {
 
     // send messages when new clients connect...
     io.emit("connect message", client.id);
-    
+
     // send chat messages to all listening clients...
-    client.on("chat message", function(msg) {
+    client.on("chat message", function (msg) {
       io.emit("chat message", msg);
     });
-    
+
     // send messages when new clients disconnect...
-    client.on("disconnect", function() {
+    client.on("disconnect", function () {
       io.emit("disconnect message", client.id);
     });
   });
